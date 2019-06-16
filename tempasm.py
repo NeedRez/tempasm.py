632efb79c269850d0fe04635279bcf77f2de161d
Kelly <30411035+NeedRez@users.noreply.github.com>
2019-06-15 19:53:56 -0700

    Update tempasm.py

--- tempasm.py
+++ tempasm.py
@@ -11,36 +11,36 @@ def main():
     prbegin = 'print(r"""'
     prendnoformat = '"""),\n'
     prendinsection = '""".format(**locals())),\n'
-    parser = ArgumentParser(description='Template engine for assembly code')
-    parser.add_argument('filename', help='blah.s')
+    argparser = ArgumentParser(description='Template engine for assembly code')
+    argparser.add_argument('filename', help='blah.s')
     indent = 0
     with open('tmpxme.py', 'w') as d:
-        with open(parser.parse_args().filename) as s:
+        with open(argparser.parse_args().filename) as s:
             d.write(prbegin)
             insideprint = True
             insidesect = False
             insidecode = False
             for line in s:
-                esc = re.search(r'^\.(.)(.*)',line)
-                if esc:
-                    if insideprint:     # going from inside to outside (esc is outside)
+                lineparse = re.search(r'^\.([>/\\{}])(.*)',line)
+                if lineparse:
+                    if insideprint:     # going from inside to outside (lineparse is outside)
                         insideprint = False
                         if insidesect:
                             d.write(prendinsection)
                         else:
                             d.write(prendnoformat)
-                    if esc.group(1) == '\\':    # python indent +1 level
-                        d.write(' '*indent + esc.group(2)+'\n')
+                    if lineparse.group(1) == '\\':    # python indent +1 level
+                        d.write(' '*indent + lineparse.group(2)+'\n')
                         insidesect = True
                         indent += 4
-                    if esc.group(1) == '/':     # python unindent 1 level
+                    if lineparse.group(1) == '/':     # python unindent 1 level
                         insidesect = False
                         indent -= 4
-                    if esc.group(1) == '>':     # insert python command
-                        d.write(' '*indent + esc.group(2)+'\n')
-                    if esc.group(1) == '}':     # start of python code section
+                    if lineparse.group(1) == '>':     # insert python command
+                        d.write(' '*indent + lineparse.group(2)+'\n')
+                    if lineparse.group(1) == '}':     # start of python code section
                         insidecode = False
-                    if esc.group(1) == '{':     # end of python code section
+                    if lineparse.group(1) == '{':     # end of python code section
                         insidecode = True
                 else:
                     if not insideprint and not insidecode:
