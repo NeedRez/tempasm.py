d0eb18c91d0dee946beec755fa89c85d10cc6ac7
Kelly <30411035+NeedRez@users.noreply.github.com>
2019-06-23 13:36:49 -0700

    Update tempasm.py

--- tempasm.py
+++ tempasm.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
-#	filename: tempasm.py
+#	sourcefile: tempasm.py
 #	author: kjp@kellyfx.com
 #	desc: converts assembly template file into an assembly code generator python script, then runs it
 #           ultimate purpose is to make asm() function from assembly, probably from .lss
 #           yes this is a code generator that creates code generators
-#   TODO: bitfield needs a regex search group for before brackets
-#   TODO: give argparse hint that this is file
-#   TODO: a bit touchy how you end the file probably needs a quote close
-#   TODO: ability to wrap each line with quotes to be inserted in asm() from .< directive
 
-from argparse import ArgumentParser
+#from argparse import ArgumentParser, FileType
+import argparse
 import re, sys, string
 
 def main():
@@ -18,13 +15,13 @@ def main():
     prbegin = 'sys.stdout.write(r"""'
     prendnoformat = '""")\n'
     prendinsection = '""".format(**locals()))\n'
-    argparser = ArgumentParser(description='Template engine for assembly code')
-    argparser.add_argument('filename', help='blah.s')
+    argparser = argparse.ArgumentParser(description='Template engine for assembly code')
+    argparser.add_argument('sourcefile', help='blah.s', type=argparse.FileType('r'))
     indent = 0
-    with open(argparser.parse_args().filename, mode='r') as f:
+    with argparser.parse_args().sourcefile as f:
         src = f.readlines()
     with open('tmpxme.py', mode='w') as dest:
-        dest.write(header % argparser.parse_args().filename)
+        dest.write(header % argparser.parse_args().sourcefile.name)
         insideprint = False
         insidesect = False
         insidecode = False
@@ -37,32 +34,36 @@ def main():
                         dest.write(prendinsection)
                     else:
                         dest.write(prendnoformat)
-                if lineparse.group(1) == '\\':    # python indent +1 level
-                    dest.write(' '*indent + lineparse.group(2))
-                    if lineparse.group(2)[-1] == ':':
+                if lineparse.group(1) == '\\':      # python indent +1 level
+                    statement = lineparse.group(2)
+                    if statement == '':
+                        statement = 'if 1:'         # faking an indent, hakception for simply interpret the text
+                    dest.write(' '*indent + statement)
+                    if statement[-1] == ':':        # is this indent or function
                         dest.write('\n')
                         insidesect = True
                     else:
                         dest.write(fnbegin)
                         insideprint = True
                     indent += 4
-                if lineparse.group(1) == '/':     # python unindent 1 level
+                if lineparse.group(1) == '/':       # python unindent 1 level
                     indent -= 4
                     if indent <= 0:
                         indent = 0
                         insidesect = False
-                if lineparse.group(1) == '>':     # insert python command
+                if lineparse.group(1) == '>':       # insert python command
                     dest.write(' '*indent + lineparse.group(2)+'\n')
-                if lineparse.group(1) == '}':     # start of python code section
+                if lineparse.group(1) == '}':       # start of python code section
                     insidecode = False
-                if lineparse.group(1) == '{':     # end of python code section
+                if lineparse.group(1) == '{':       # end of python code section
                     insidecode = True
             else:
                 if not insideprint and not insidecode:
                     dest.write(' '*indent + prbegin)
                     insideprint = True
                 dest.write(line)
-        dest.write(prendnoformat)
+        if insideprint:
+            dest.write(prendnoformat)
     import tmpxme
 
 if __name__ == "__main__":
